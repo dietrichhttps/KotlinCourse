e50d70e080d0818c9166d935d34d1de8b2284acb
aliceglassB660M <abayhanov1998@gmail.com>
2024-12-09 22:34:38 +0300

    Elvis. Unit.

--- OOP/src/corporation/Corporation.kt
+++ OOP/src/corporation/Corporation.kt
@@ -3,6 +3,17 @@ package corporation
 fun main() {
 //    val accountant = Accountant(100,"Mike", 60, 50000)
 //    accountant.work()
-    val assistant = WorkersRepository.findAssistant()
-    assistant?.printInfo()
+//    val assistant = WorkersRepository.findAssistant()
+//    val director = WorkersRepository.findDirector()
+//    assistant?.printInfo()
+//    director?.printInfo()
+//    val assistantSalary = assistant?.salary ?: 0
+//    val directorSalary = director?.salary ?: 0
+//    val sum = assistantSalary + directorSalary
+    val a = method()
+    println(a.hashCode())
+}
+
+fun method() {
+
 }
