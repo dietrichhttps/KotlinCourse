808c3f3be0e5ba22d1290bb826d2d8ae1942206a
aliceglassT480 <abayhanov1998@gmail.com>
2024-12-13 19:18:28 +0300

    Introduction to Generics

--- FunctionalProgramming/src/main/kotlin/homework/product/Store.kt
+++ FunctionalProgramming/src/main/kotlin/homework/product/Store.kt
@@ -2,10 +2,19 @@ package homework.product
 
 fun main() {
     val productCards = ProductCardsRepository.productCards
-    var filteredProductCards = filter(productCards) { it.price > 500 }
-    filteredProductCards = filter(filteredProductCards) { it.rating > 4 }
-    filteredProductCards = filter(filteredProductCards) { it.category == ProductCategory.SPORTS }
-    filteredProductCards.forEach { println(it) }
+    val filteredProductCards = filter(productCards) { it.category == ProductCategory.CLOTHING }
+    val doubledPrice = transform(filteredProductCards) { it.copy(price = it.price * 2) }
+    val transformedToString = transform(doubledPrice) { "${it.id} - ${it.name} - ${it.price}" }
+    transformedToString.forEach { println(it) }
+
+}
+
+fun <R> transform(productCards: List<ProductCard>, operation: (ProductCard) -> R): List<R> {
+    val result = mutableListOf<R>()
+    for (productCard in productCards) {
+        result.add(operation(productCard))
+    }
+    return result
 }
 
 fun filter(productCards: List<ProductCard>, isSuitable: (ProductCard) -> Boolean): List<ProductCard> {
--- FunctionalProgramming/src/main/kotlin/profile/Profile.kt
+++ FunctionalProgramming/src/main/kotlin/profile/Profile.kt
@@ -5,7 +5,16 @@ fun main() {
     var filteredProfiles = filter(profiles) { it.age > 25 }
     filteredProfiles = filter(filteredProfiles) { it.gender == Gender.MALE }
     filteredProfiles = filter(filteredProfiles) { it.firstName.startsWith("A") }
-    filteredProfiles.forEach { println(it) }
+    val transformed = transform(filteredProfiles) { it.copy(age = it.age + 1) }
+    transformed.forEach { println(it) }
+}
+
+fun <R> transform(profiles: List<Person>, operation: (Person) -> R): List<R> {
+    val result = mutableListOf<R>()
+    for (person in profiles) {
+        result.add(operation(person))
+    }
+    return result
 }
 
 fun filter(profiles: List<Person>, isSuitable: (Person) -> Boolean): List<Person> {
