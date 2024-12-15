62d89bb3dd4175d2c18be5aa6ffb5124575a11f5
aliceglassB660M <abayhanov1998@gmail.com>
2024-12-15 21:31:38 +0300

    Map for improved performance

--- FunctionalProgramming/src/main/kotlin/dictionary/Dictionary.kt
+++ FunctionalProgramming/src/main/kotlin/dictionary/Dictionary.kt
@@ -1,20 +1,25 @@
 package dictionary
 
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
+import kotlin.time.measureTime
 
 fun main() {
     val file = File("dictionary.json")
     val content = file.readText().trim()
-    val dictionary = Json.decodeFromString<List<Entry>>(content)
+    val dictionary = Json.decodeFromString<Map<String, String>>(content)
     showDescription(dictionary)
 }
 
-fun showDescription(dictionary: List<Entry>) {
+fun showDescription(dictionary: Map<String, String>) {
     while (true) {
         print("Enter word or 0 to exit: ")
-        val input = readln()
+        val input = readln().trim().lowercase()
         if (input == "0") break
-        dictionary.find { input.lowercase() == it.value }?.let { println(it.description) } ?: println("Word not found")
+        val time = measureTime {
+            dictionary[input]?.let { println(it) } ?: println("Word not found")
+        }
+        println("Duration: $time")
     }
 }
