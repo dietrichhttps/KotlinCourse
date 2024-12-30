c4136fbbaf74c192321cefdb1b4211d92b0a43e3
aliceglassB660M <abayhanov1998@gmail.com>
2024-12-30 22:06:34 +0300

    Set implementations

--- Collections/src/main/kotlin/collections/Item.kt
+++ Collections/src/main/kotlin/collections/Item.kt
@@ -1,24 +1,5 @@
 package collections
 
-class Item (
+data class Item(
     val value: Int
-) {
-     override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Item
-
-        return value == other.value
-    }
-
-    override fun hashCode(): Int {
-        return value
-    }
-
-    override fun toString(): String {
-        return "Item(value=$value)"
-    }
-
-
-}
+)
--- Collections/src/main/kotlin/collections/Test.kt
+++ Collections/src/main/kotlin/collections/Test.kt
@@ -3,14 +3,20 @@ package collections
 import kotlin.random.Random
 
 fun main() {
-    val elements = MyHashSet<Item>().apply {
-        repeat(100) {
-            add(Item(Random.nextInt(1000)))
+    val elements = sortedSetOf<Item>({ o1, o2 ->
+        when {
+            o1.value > o2.value -> 1
+            o1.value < o2.value -> -1
+            else -> 0
         }
-    }
+    })
+        .apply {
+            repeat(100) {
+                add(Item(Random.nextInt(100)))
+            }
+        }
 
     for (element in elements) {
         println(element)
-//        elements.add(Item(100))
     }
 }
