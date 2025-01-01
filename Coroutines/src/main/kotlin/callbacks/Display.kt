f8f998b150d705112b2264639c3badccf5280ce7
aliceglassB660M <abayhanov1998@gmail.com>
2025-01-01 16:23:09 +0300

    Adding logic and the String.format method

--- Coroutines/src/main/kotlin/callbacks/Display.kt
+++ Coroutines/src/main/kotlin/callbacks/Display.kt
@@ -1,5 +1,7 @@
 package callbacks
 
+import entities.Author
+import entities.Book
 import java.awt.BorderLayout
 import java.awt.Dimension
 import javax.swing.*
@@ -10,7 +12,18 @@ object Display {
         isEditable = false
     }
 
-    private val loadButton = JButton("Load book")
+    private val loadButton = JButton("Load book").apply {
+        addActionListener {
+            isEnabled = false
+            infoArea.text = "Loading book information...\n"
+            val book = loadBook()
+            infoArea.append("Book: ${book.title}\nYear: ${book.year}\nGenre: ${book.genre}\n")
+            infoArea.append("Loading author information...\n")
+            val author = loadAuthor(book)
+            infoArea.append("Author: ${author.name}\nBio: ${author.bio}\n")
+            isEnabled = true
+        }
+    }
     private val timerLabel = JLabel("Time: 00:00")
     private val topPanel = JPanel(BorderLayout()).apply {
         add(timerLabel, BorderLayout.WEST)
@@ -26,6 +39,28 @@ object Display {
 
     fun show() {
         mainFrame.isVisible = true
+        startTimer()
+    }
+
+    private fun loadBook(): Book {
+        Thread.sleep(3000)
+        return Book("1984", 1949, "Dystopia")
+    }
+
+    private fun loadAuthor(book: Book): Author {
+        Thread.sleep(3000)
+        return Author("George Orwell", "British writer and journalist")
+    }
+
+    private fun startTimer() {
+        var totalSeconds = 0
+        while (true) {
+            val minutes = totalSeconds / 60
+            val seconds = totalSeconds % 60
+            timerLabel.text = String.format("Time: %02d:%02d", minutes, seconds)
+            Thread.sleep(1000)
+            totalSeconds++
+        }
     }
 }
 
