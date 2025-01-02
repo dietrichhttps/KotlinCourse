c3488ed986f1cd0b026ccf35d23fc45efe91ba96
aliceglassB660M <abayhanov1998@gmail.com>
2025-01-02 13:45:06 +0300

    Executor Services

---
+++ Coroutines/src/main/kotlin/executors/Test.kt
@@ -0,0 +1,20 @@
+package executors
+
+import java.util.concurrent.Executors
+
+fun main() {
+    val executorService = Executors.newFixedThreadPool(2000)
+    repeat(10_000) {
+        executorService.execute {
+            processImage(Image(it))
+        }
+    }
+}
+
+private fun processImage(image: Image) {
+    println("Image $image is processing")
+    Thread.sleep(1000)
+    println("Image $image processed")
+}
+
+data class Image(val id: Int)
