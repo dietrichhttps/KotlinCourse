73495d052f70bac16917a0fc48c89d4511a711aa
aliceglassB660M <abayhanov1998@gmail.com>
2025-01-03 18:57:28 +0300

    SupervisorJob

---
+++ Coroutines/src/main/kotlin/supervisorJob/Test.kt
@@ -0,0 +1,28 @@
+package supervisorJob
+
+import kotlinx.coroutines.*
+import java.util.concurrent.Executors
+
+private val parentJob = SupervisorJob()
+private val exceptionHandler = CoroutineExceptionHandler { _, _ ->
+    println("Exception caught")
+}
+private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
+private val scope = CoroutineScope(dispatcher + parentJob + exceptionHandler)
+
+fun main() {
+    scope.launch {
+        longOperation(3000, 1)
+        error("")
+    }
+
+    scope.launch {
+        longOperation(4000, 2)
+    }
+}
+
+private suspend fun longOperation(timeMillis: Long, number: Int) {
+    println("Started: $number")
+    delay(timeMillis)
+    println("Finished: $number")
+}
