d18fe41455e0be28211674f409313d2ccee08b20
aliceglassB660M <abayhanov1998@gmail.com>
2025-01-04 14:15:18 +0300

    Introduction to the API

--- FlowAndChannels/build.gradle.kts
+++ FlowAndChannels/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 group = "org.example"
@@ -10,6 +11,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
     testImplementation(kotlin("test"))
 }
---
+++ FlowAndChannels/src/main/kotlin/dictionary/Repository.kt
@@ -0,0 +1,8 @@
+package dictionary
+
+object Repository {
+
+    private const val BASE_URL = "https://api.api-ninjas.com/v1/dictionary?word"
+    private const val API_KEY = "inatJobnk7PKjhFumlB2pg==4CBa2kLxGLQBtZ3O"
+    private const val HEADER_KEY = "X-Api-Key"
+}
