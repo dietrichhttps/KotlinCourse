53f5089edf8891eed9d48bea864a351d6235d500
aliceglassB660M <abayhanov1998@gmail.com>
2025-01-04 21:53:05 +0300

    Cold Flows

---
+++ FlowAndChannels/src/main/kotlin/coldFlows/Test.kt
@@ -0,0 +1,25 @@
+package coldFlows
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executors
+
+private val scope = CoroutineScope(Executors.newCachedThreadPool().asCoroutineDispatcher())
+
+fun main() {
+    val flow = Repository.timer
+    scope.launch {
+//        flow.take(3)
+//            .collect {
+//                println(it)
+//            }
+    }
+    scope.launch {
+        flow
+            .collect {
+                println(it)
+            }
+        println("Finished")
+    }
+}
