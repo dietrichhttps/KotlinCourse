262ccb4da123bc90f3f0ac3b6e35c876813f041e
aliceglassPC <abayhanov1998@gmail.com>
2025-01-11 19:10:25 +0300

    Initial commit

---
+++ Collections/src/main/kotlin/generics/Test.kt
@@ -0,0 +1,24 @@
+package generics
+
+import collections.MyList
+import collections.myListOf
+
+fun main() {
+    val worker = Programmer("John")
+    showName(worker)
+
+    val workers: MyList<Programmer> = myListOf(Programmer("Nick"), Programmer("Ivan"))
+    showCount(workers)
+}
+
+private fun showCount(workers: MyList<Worker>) {
+    println(workers.size)
+}
+
+private fun showName(worker: Worker) {
+    println(worker.name)
+}
+
+open class Worker(val name: String)
+
+class Programmer(name: String) : Worker(name)
