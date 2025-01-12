e8291940bcd2ceb6614981e0684d8e266265d64c
aliceglassPC <abayhanov1998@gmail.com>
2025-01-12 19:25:45 +0300

    Memory in Kotlin

---
+++ Collections/src/main/kotlin/memory/Test.kt
@@ -0,0 +1,18 @@
+package memory
+
+import collections.MyLinkedList
+
+data class User(val name: String)
+
+fun main() {
+//    var list = MyLinkedList<User>()
+//    repeat(3) {
+//        list.add(User("$it"))
+//    }
+//    list = MyLinkedList()
+
+    val a: Int = 10
+    val b: Int? = 5
+    println(a)
+    println(b)
+}
